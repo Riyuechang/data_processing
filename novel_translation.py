fb3f6415d0a53d5286b5e14cf2bead9b5d8a7897
Riyuechang <tsps99187@gmail.com>
2025-08-21 15:15:25 +0800

    新增術語表功能

--- novel_translation.py
+++ novel_translation.py
@@ -11,6 +11,7 @@ MAX_TOKENS = 2048 #8192
 TEMPERATURE = 0.1
 MIN_P = 0.1
 TOP_P = 0.3
+FREQUENCY_PENALTY = 0.2
 
 MAX_REQUESTS = 32 #8 64 128 256 
 MAX_BATCHED_TOKENS = 32768 #8192 16384 65536
@@ -23,8 +24,12 @@ MODEL_PATH = f"/media/ifw/GameFile/linux_cache/LLMModel/{MODEL_NAME}"
 TOKENIZER_NAME = "Sakura-1.5B-Qwen2.5-v1.0-HF"
 TOKENIZER_PATH = f"/media/ifw/GameFile/linux_cache/LLMModel/{TOKENIZER_NAME}"
 
+#NOVEL_NAME = "test"
 NOVEL_NAME = "Heru_modo_Yarikomizuki_no_gema_v01-06_epub"
 NOVEL_PATH = f"./output/{NOVEL_NAME}"
+
+USE_GLOSSARY = True
+GLOSSARY_PATH = "./translation/sakura_gpt_dict.json"
 
 SAVE_DIR_PATH = f"./translation/{NOVEL_NAME}"
 
@@ -33,9 +38,7 @@ def vllm_add_request(input_text: str, request_id: str):
     prompt = tokenizer.apply_chat_template(
         [
             {"role": "system", "content": "你是一个轻小说翻译模型，可以流畅通顺地以日本轻小说的风格将日文翻译成简体中文，并联系上下文正确使用人称代词，不擅自添加原文中没有的代词。"},
-            #{"role": "system", "content": "你是一个视觉小说翻译模型，可以通顺地使用给定的术语表以指定的风格将日文翻译成简体中文，并联系上下文正确使用人称代词，注意不要混淆使役态和被动态的主语和宾语，不要擅自添加原文中没有的特殊符号，也不要擅自增加或减少换行。"},
-            {"role": "user", "content": f"将下面的日文文本翻译成中文：{input_text}"}
-            #{"role": "user", "content": f"参考以下术语表（可为空，格式为src->dst #备注）：\n\n根据以上术语表的对应关系和备注，结合历史剧情和上下文，将下面的文本从日文翻译成简体中文：\n{input_text}"}
+            {"role": "user", "content": f"根据以下术语表（可以为空）：\n{glossary_prompt}\n将下面的日文文本翻译成中文：{input_text}" if USE_GLOSSARY else f"将下面的日文文本翻译成中文：{input_text}"}
         ], 
         add_generation_prompt=True, 
         tokenize=False
@@ -47,6 +50,7 @@ def vllm_add_request(input_text: str, request_id: str):
         min_p=MIN_P,
         top_p=TOP_P,
         #top_k=50,
+        frequency_penalty=FREQUENCY_PENALTY,
         #seed=None
     )
     llm_engine.add_request(
@@ -79,6 +83,16 @@ if not os.path.isdir(SAVE_DIR_PATH):
     os.mkdir(SAVE_DIR_PATH)
 
 novel_file_list = [dir for dir in os.listdir(NOVEL_PATH) if dir.endswith(".json")]
+
+if USE_GLOSSARY:
+    with open(GLOSSARY_PATH, "r", encoding="utf-8") as file:
+        glossary_dict: list[dict[str, str]] = json.load(file)
+    
+    glossary_list = [
+        f"{glossary['jp']}->{glossary['tw']} #{glossary['info']}" if glossary["info"] else f"{glossary['jp']}->{glossary['tw']}"
+        for glossary in glossary_dict
+    ]
+    glossary_prompt = "\n".join(glossary_list)
 
 tqdm_progress = tqdm(novel_file_list)
 for novel_file in tqdm_progress:
