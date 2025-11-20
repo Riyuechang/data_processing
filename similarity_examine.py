61841729f40479161f7b39cf88923f0d2ced9e40
Riyuechang <tsps99187@gmail.com>
2025-11-20 14:25:21 +0800

    更改使用方式和參數更新

--- similarity_examine.py
+++ similarity_examine.py
@@ -1,20 +1,21 @@
 import json
 
 
-MAX_JP_VALUE = 0.2
-MAX_TW_VALUE = 0.6
+NUM = 1
+
+MAX_JP_VALUE = 0.15
+MAX_TW_VALUE = 0.75
 MAX_SUB_JP_VALUE = 0.05
-MAX_SUB_TW_VALUE = 0.9
+MAX_SUB_TW_VALUE = 0.95
 
 FIRST_MATCH_WORDS = ["※", "*", "【", "】"]
 SECOND_TOP_MATCH_WORDS = [":", "："]
-SECOND_MATCH_WORDS = ["注", "原", "译", "这句", "这条", "这行", "这段", "这边", "这个", "这里", "此处", "日文", "ps", "PS", "Ps", "pS", "(", "（", ")", "）"] # 第 卷 话
+SECOND_MATCH_WORDS = ["㊟", "注", "编", "原", "译", "这句", "这条", "这行", "这段", "这边", "这个", "这里", "此处", "日文", "ps", "PS", "Ps", "pS", "(", "（", ")", "）"] # 第 卷 话
 
-NOVEL_NAME = "關於我在無意間被隔壁的天使變成廢柴這件事_5"
-JP_TW_NOVEL_SIMILARITY_PATH = f"/media/ifw/GameFile/linux_cache/data_processed/jp_tw_novel_similarity/{NOVEL_NAME}_similarity.json"
+NOVEL_NAME = f"關於我在無意間被隔壁的天使變成廢柴這件事_v01-10_similarity/關於我在無意間被隔壁的天使變成廢柴這件事_{NUM}.json"
+JP_TW_NOVEL_SIMILARITY_PATH = f"/media/ifw/GameFile/linux_cache/data_processed/{NOVEL_NAME}"
 
-DATA_OUTPUT_PATH = f"/media/ifw/GameFile/linux_cache/data_processed/jp_tw_novel_examine/{NOVEL_NAME}_examine.json"
-
+DATA_OUTPUT_PATH = f"/media/ifw/GameFile/linux_cache/data_processed/關於我在無意間被隔壁的天使變成廢柴這件事_v01-10_examine/{NOVEL_NAME.split('/')[-1]}"
 
 with open(JP_TW_NOVEL_SIMILARITY_PATH, "r", encoding="utf-8") as file:
     dataset: list[dict[str, str]] = json.load(file)
@@ -25,25 +26,26 @@ for chapter in dataset:
             any(word in alignment["tw"] for word in SECOND_TOP_MATCH_WORDS) 
             and any(word in alignment["tw"] for word in SECOND_MATCH_WORDS)
         ):
-            alignment["examine"] = True
+            alignment["examine"] = "True_匹配"
             continue
 
-        if (alignment["jp"][-1] != alignment["tw"][-1] 
-            and (
+        if ((alignment["jp"][-1] != alignment["tw"][-1] 
+             or alignment["translation"][-1] != alignment["tw"][-1]
+            )and (
                 alignment["similarity_to_jp"] > MAX_SUB_JP_VALUE
                 or alignment["similarity_translation"] < MAX_SUB_TW_VALUE
             )
         ):
-            alignment["examine"] = True
+            alignment["examine"] = "True_尾字不同"
             continue
 
         if (alignment["similarity_to_jp"] > MAX_JP_VALUE 
             or alignment["similarity_translation"] < MAX_TW_VALUE
         ):
-            alignment["examine"] = True
+            alignment["examine"] = "True_小於"
             continue
 
-        alignment["examine"] = False
+        alignment["examine"] = "False"
 
 with open(DATA_OUTPUT_PATH, 'w', encoding='utf-8') as file:
     json.dump(dataset, file, indent=4, ensure_ascii=False)
--- similarity_marking.py
+++ similarity_marking.py
@@ -4,14 +4,16 @@ from tqdm import tqdm
 from sentence_transformers import SentenceTransformer
 
 
+NUM = 1
+
 BATCH_SIZE = 64
 
 MODEL_PATH = "/media/ifw/GameFile/linux_cache/embedding_model/jina-embeddings-v3"
 
-NOVEL_NAME = "關於我在無意間被隔壁的天使變成廢柴這件事_6"
-JP_TW_NOVEL_TRANSLATION_ALIGNMENT_PATH = f"/media/ifw/GameFile/linux_cache/data_processed/jp_tw_novel_translation_alignment/{NOVEL_NAME}_translation.json"
+NOVEL_NAME = f"關於我在無意間被隔壁的天使變成廢柴這件事_v01-10_translation_alignment/關於我在無意間被隔壁的天使變成廢柴這件事_{NUM}.json"
+JP_TW_NOVEL_TRANSLATION_ALIGNMENT_PATH = f"/media/ifw/GameFile/linux_cache/data_processed/{NOVEL_NAME}"
 
-DATA_OUTPUT_PATH = f"/media/ifw/GameFile/linux_cache/data_processed/jp_tw_novel_similarity/{NOVEL_NAME}_similarity.json"
+DATA_OUTPUT_PATH = f"/media/ifw/GameFile/linux_cache/data_processed/關於我在無意間被隔壁的天使變成廢柴這件事_v01-10_similarity/{NOVEL_NAME.split('/')[-1]}"
 
 
 model = SentenceTransformer(MODEL_PATH, trust_remote_code=True)
@@ -24,7 +26,7 @@ for chapter in tqdm(dataset, desc="正在計算相似度..."):
     for alignment in chapter["alignment"]:
         sentences.append(alignment["jp"])
         sentences.append(alignment["tw"])
-        sentences.append(alignment["tw_translation"])
+        sentences.append(alignment["translation"])
 
     embeddings = model.encode(sentences, batch_size=BATCH_SIZE, convert_to_tensor=True)
 
@@ -32,10 +34,10 @@ for chapter in tqdm(dataset, desc="正在計算相似度..."):
     for alignment in chapter["alignment"]:
         jp_embedding = next(embeddings_iter)
         tw_embedding = next(embeddings_iter)
-        tw_translation_embedding = next(embeddings_iter)
+        translation_embedding = next(embeddings_iter)
 
-        alignment["similarity_to_jp"] = abs(float(model.similarity(jp_embedding, tw_embedding)[0][0]) - float(model.similarity(jp_embedding, tw_translation_embedding)[0][0]))
-        alignment["similarity_translation"] = float(model.similarity(tw_embedding, tw_translation_embedding)[0][0])
+        alignment["similarity_to_jp"] = abs(float(model.similarity(jp_embedding, tw_embedding)[0][0]) - float(model.similarity(jp_embedding, translation_embedding)[0][0]))
+        alignment["similarity_translation"] = float(model.similarity(tw_embedding, translation_embedding)[0][0])
 
 with open(DATA_OUTPUT_PATH, 'w', encoding='utf-8') as file:
     json.dump(dataset, file, indent=4, ensure_ascii=False)
